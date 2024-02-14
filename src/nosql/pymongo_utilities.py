3b7f716a1d94f0fec6b69ad52fa8eb2ce2e4f5ba
kvlinden <kvlinden@calvin.edu>
2024-02-14 17:08:11 -0500

    building module 7

--- src/nosql/pymongo_utilities.py
+++ src/nosql/pymongo_utilities.py
@@ -1,16 +1,16 @@
 """
 Utilities for PyMongoDB operations - Reference:
-    - https://www.mongodb.com/docs/guides/
+- https://www.mongodb.com/docs/guides/
 
 Notes:
-    - Build the collections using `pymongo_load.py` and
-        read them using `pymongo_read.py`.
-    - Installed 'pymongo[srv]' as specified in the guide.
-    - TODO: Auto-set the user ID/password in the production environment.
-        See: https://docs.github.com/en/codespaces/managing-your-codespaces/managing-your-account-specific-secrets-for-github-codespaces
-
+- Build the collections using `pymongo_load.py` and
+    read them using `pymongo_read.py`.
+- Installed 'pymongo[srv]' as specified in the guide.
+- TODO: Auto-set the user ID/password in the production environment.
+    See: https://docs.github.com/en/codespaces/managing-your-codespaces/managing-your-account-specific-secrets-for-github-codespaces
+- I had to add my IP address to the MongoDB cloud database's "Network Access" 
+    settings, which worked for multiple machines.
 """
-import getpass
 import os
 from pymongo import MongoClient
 
